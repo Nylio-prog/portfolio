5a0737eb6266d918932aacb3dde6b6e839f80385
Nylio-prog <nils.cercariolo@gmail.com>
2023-06-19 00:55:14 +0900

    added HOC for social medias + copyright + navDots

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,18 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
-
-import { images } from '../../constants';
+import { urlFor, client} from '../../client'
 
 import './About.scss';
 
-const abouts = [
-  { title: 'Web development', description: 'I am a good web developer.', imgUrl: images.about01},
-  { title: 'Front end', description: 'I am a good web developer.', imgUrl: images.about02},
-  { title: 'Back End', description: 'I am a good web developer.', imgUrl: images.about03},
-  { title: 'AI', description: 'I am a good web developer.', imgUrl: images.about04},
-]
+const About = () => { 
+  const [abouts, setAbouts] = useState([]);
 
-const About = () => {
+  useEffect(() => {
+    const query = `*[_type == "abouts"]`;
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, [])
+  
+
   return (
     <>
     <h2 className='head-text'>I Know That <span> Good development</span><br /> means<span> Good business</span>
@@ -27,7 +29,7 @@ const About = () => {
           className='app__profile-item'
           key={about.title + index}
         >
-          <img src={about.imgUrl} alt={about.title}/>
+          <img src={urlFor(about.imgUrl)} alt={about.title}/>
           <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
           <h2 className='bold-text' style={{marginTop: 10}}>{about.description}</h2>
         </motion.div>
