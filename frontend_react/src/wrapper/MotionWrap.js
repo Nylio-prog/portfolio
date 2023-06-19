6cf8287cd3a93b3f3dc1a96678df0670871a20c3
Nylio-prog <nils.cercariolo@gmail.com>
2023-06-19 17:02:12 +0900

    finished testimonials

--- frontend_react/src/wrapper/MotionWrap.js
+++ frontend_react/src/wrapper/MotionWrap.js
@@ -6,7 +6,7 @@ const MotionWrap = (Component, className) => function HOC() {
     <motion.div
         whileInView={{y: [100, 50, 0], opacity: [0, 0, 1]}}
         transition={{duration: 0.5}}
-        className={`${className} app_flex`}
+        className={`${className} app__flex`}
     >
         <Component />
     </motion.div>
