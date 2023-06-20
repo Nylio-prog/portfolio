41df455c34cbdcf78d09e9a2d0b0518f1aae731d
Nylio-prog <nils.cercariolo@gmail.com>
2023-06-20 16:38:44 +0900

    #4 modified social medias

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
-            <BsTwitter />
+          <a href='https://www.linkedin.com/in/nils-cercariolo' target='_blank' rel="noreferrer">
+            <BsLinkedin />
+          </a>
         </div>
         <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
+          <a href='https://github.com/Nylio-prog' target='_blank' rel="noreferrer">
+            <BsGithub />
+          </a>
         </div>
     </div>
   )
